80af92abd2a5cef03a371f823dae30ffa9de9208
furkancak1r <furkancakr7@gmail.com>
2023-09-21 16:00:33 +0300

    3. Commit - 21.09.2023
    "Birim Sarf Miktar", "Toplam Sarf Miktar" , "Notlar" are added. B2 and C2 cells are merged.

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ excel = win32.gencache.EnsureDispatch('Excel.Application')
 excel.Visible = True  # Excel penceresini görünür yap
 
 # A2'de "Notlar" yazısını ekleyen fonksiyon
+
+
 def add_notes_title(worksheet):
     cell = worksheet.Cells(2, 1)  # A2 hücresini seç
     cell.Value = "Notlar"  # Hücreye "Notlar" yazısı ekle
@@ -15,14 +17,20 @@ def add_notes_title(worksheet):
     worksheet.Range("B2:C2").Merge()  # B2 ve C2 hücrelerini birleştir
 
 # A3'den D'deki en son satıra kadar olan hücrelere kenarlık eklemek için fonksiyon
+
+
 def add_border_to_range(worksheet, start_cell, end_cell):
     range_to_border = worksheet.Range(start_cell, end_cell)
     range_to_border.Borders.LineStyle = 1  # Kenarlık çizgilerini ince olarak ayarla
 
-# A3'den D3'e kadar olan verileri bir diziye ekleyin
-header = ["Malzeme Kodu", "Malzeme Aciklamasi", "Sarf Miktar", "Birim"]
+
+# A3'den E3'e kadar olan verileri bir diziye ekleyin
+header = ["Malzeme Kodu", "Malzeme Açıklaması",
+          "Birim Sarf Miktar", "Toplam Sarf Miktar", "Birim"]
 
 # Excel dosyasını oluşturmak için fonksiyon
+
+
 def create_excel():
     excel_filename = excel_filename_entry.get()
     copied_text = root.clipboard_get()  # Kopyalanan metni al
@@ -33,9 +41,12 @@ def create_excel():
     worksheet = workbook.Worksheets(1)
     worksheet.Range("A:E").VerticalAlignment = -4108  # Dikeyde ortala
     worksheet.Range("A:B").HorizontalAlignment = -4108  # A kolonunu ortala
-    worksheet.Range("B:C").HorizontalAlignment = -4131  # B kolonunu sola dayalı
-    worksheet.Range("C:D").HorizontalAlignment = -4152  # A kolonunu sağa dayalı
-    worksheet.Range("D:E").HorizontalAlignment = -4108  # A kolonunu ortala hizala
+    worksheet.Range("B:C").HorizontalAlignment = - \
+        4131  # B kolonunu sola dayalı
+    worksheet.Range("C:D").HorizontalAlignment = - \
+        4152  # A kolonunu sağa dayalı
+    worksheet.Range("D:E").HorizontalAlignment = - \
+        4108  # A kolonunu ortala hizala
     worksheet.Range("A3:E3").HorizontalAlignment = -4108
     worksheet.Range("A3:E3").VerticalAlignment = -4108
     # Başlık verilerini A3'den D3'e yerleştirin
@@ -51,24 +62,39 @@ def create_excel():
 
         for value in values:  # Satırdaki her değer için
             if values != ['']:  # tab'dan kalan son boşluğu es geçmek için
-                cell = worksheet.Cells(row, col)  # Hücreyi seç
-                if col == 3:
-                    # Öncelikle hücre biçimini metin olarak ayarla çünkü diğer türlü uzun sayılarda virgül yok oluyor
-                    cell.NumberFormat = "@"
-                cell.Value = value  # Hücreye değeri yaz
-                if col == 3:
-                    # Ondalık biçimini ayarla (nokta ile)
-                    cell.NumberFormat = "0.00"
-                # Eğer hücre boşsa veya bu son hücre ise arka planı kırmızıya boyayın
-                if not cell.Value:
-                    cell.Interior.Color = 255  # Kırmızı rengi temsil eden değer
+                worksheet.Cells(row, 4).NumberFormat = "@" # Öncelikle hücre biçimini metin olarak ayarla çünkü diğer türlü uzun sayılarda virgül yok oluyor
+
+
+                if col == 1:
+                    worksheet.Cells(row, 1).Value = value  # Hücreye değeri yaz
+                    if not value:
+                        # Kırmızı rengi temsil eden değer
+                        worksheet.Cells(row, 1).Interior.Color = 255
+                elif col == 2:
+                    worksheet.Cells(row, 2).Value = value  # Hücreye değeri yaz
+                    if not value:
+                        # Kırmızı rengi temsil eden değer
+                        worksheet.Cells(row, 2).Interior.Color = 255
+                elif col == 3:
+                    worksheet.Cells(row, 4).Value = value  # Hücreye değeri yaz
+                    worksheet.Cells(row, 4).NumberFormat = "0.00"
+
+                    if not value:
+                        # Kırmızı rengi temsil eden değer
+                        worksheet.Cells(row, 4).Interior.Color = 255
+                elif col == 4:
+                    worksheet.Cells(row, 5).Value = value  # Hücreye değeri yaz
+                    if not value:
+                        # Kırmızı rengi temsil eden değer
+                        worksheet.Cells(row, 5).Interior.Color = 255
+
                 col += 1  # Sütunu bir artır
         # Bir sonraki satıra geçmeden önce kontrol et
         if values:
             row += 1  # Satırı bir artır
 
     # A3'den D'deki en son satıra kadar olan hücrelere kenarlık ekleyin
-    add_border_to_range(worksheet, "A3", "D" + str(row - 2))
+    add_border_to_range(worksheet, "A3", "E" + str(row - 2))
     worksheet.Columns.AutoFit()
 
     # A2'de "Notlar" yazısını ekleyin
@@ -88,16 +114,19 @@ def create_excel():
     # Programı 2 saniye sonra kapat
     root.after(1500, lambda: root.destroy())
 
+
 # Tkinter penceresini oluştur
 root = tk.Tk()
 root.geometry("400x200")
 root.title("Excel Oluştur")
 
 # "Evet" düğmesi
-yes_button = tk.Button(root, text="Evet", command=lambda: [confirmation_label.pack_forget(), yes_button.pack_forget(), excel_filename_label.pack(), excel_filename_entry.pack(), create_button.pack()])
+yes_button = tk.Button(root, text="Evet", command=lambda: [confirmation_label.pack_forget(
+), yes_button.pack_forget(), excel_filename_label.pack(), excel_filename_entry.pack(), create_button.pack()])
 
 # Kullanıcıya reçeteyi kopyaladığından emin mi diye sor
-confirmation_label = tk.Label(root, text="Reçeteyi başlıksız olarak kopyaladığınızdan emin misiniz?")
+confirmation_label = tk.Label(
+    root, text="Reçeteyi başlıksız olarak kopyaladığınızdan emin misiniz?")
 confirmation_label.pack()
 
 # Excel dosyasının adı için etiket
